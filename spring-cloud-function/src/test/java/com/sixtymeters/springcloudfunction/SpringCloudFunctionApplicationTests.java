25ae426a1492969d06c3bbf3f05881feb801b2d0
Theo Winter <theo.winter@gmail.com>
2021-11-08 11:06:29 +0100

    Added sample function and test to reverse text

--- spring-cloud-function/src/test/java/com/sixtymeters/springcloudfunction/SpringCloudFunctionApplicationTests.java
+++ spring-cloud-function/src/test/java/com/sixtymeters/springcloudfunction/SpringCloudFunctionApplicationTests.java
@@ -3,11 +3,17 @@ package com.sixtymeters.springcloudfunction;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 @SpringBootTest
 class SpringCloudFunctionApplicationTests {
 
+    private final SpringCloudFunctionApplication app = new SpringCloudFunctionApplication();
+
     @Test
-    void contextLoads() {
+    void text_reverseString_textIsReversed() {
+        String output = app.reverseString().apply("hello world");
+        assertThat(output).isEqualTo("dlrow olleh");
     }
 
 }
